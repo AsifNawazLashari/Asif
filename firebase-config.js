e80f1c848a7c6f40010c1d5d864800123a5103b6
AsifNawazLashari <asiflashari143@gmail.com>
2024-04-08 18:36:25 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDZ4IjtvPGypB84evukROyNHwBvkhGyBkM",
+    authDomain: "alif-ac574.firebaseapp.com",
+    projectId: "alif-ac574",
+    storageBucket: "alif-ac574.appspot.com",
+    messagingSenderId: "261987716644",
+    appId: "1:261987716644:web:02d0beeee8a04f998a12b1",
+    measurementId: "G-YGQ1V6489M"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
